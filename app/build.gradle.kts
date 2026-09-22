d87ad27e1a22360a9ed1f1662ba07d98d2a99cdc
TRATO-finoo <jhonnydkk@gmail.com>
2026-09-22 01:03:30 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,10 +9,12 @@ android {
 
     defaultConfig {
         applicationId = "com.tratofino.jarvis"
+
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+
+        versionCode = 3
+        versionName = "1.2"
     }
 
     buildTypes {
@@ -29,11 +31,11 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
-}
 
-dependencies {
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.activity:activity-ktx:1.10.1")
+    dependencies {
+        implementation("androidx.core:core-ktx:1.15.0")
+        implementation("androidx.appcompat:appcompat:1.7.0")
+        implementation("com.google.android.material:material:1.12.0")
+        implementation("androidx.activity:activity-ktx:1.10.1")
+    }
 }
